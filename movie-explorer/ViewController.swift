af665e10e4c1af9a511e7a3ae11d1fb506f0a0f7
Yakun Zhang <zhang.yak@northeastern.edu>
2023-11-10 23:15:10 -0800

    sprint0: create project and empty screens, might change to use swiftUI during next sprint

---
+++ movie-explorer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movie-explorer
+//
+//  Created by ESZYK on 11/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
